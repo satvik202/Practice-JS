d2e5cb1d12a793e9b5d369eb49ef1d4e810c186d
satvik <7vikpandey@gmail.com>
2023-10-31 20:55:43 +0530

    making tic tac toe and rock paper scissors

--- stopWatch/script.js
+++ stopWatch/script.js
@@ -11,12 +11,13 @@ let intervalId;
 let hrs=0;
 let mins=0;
 let secs=0;
+let milliseconds=0;
 
 startBtn.addEventListener('click', () => {
     if(paused){
         paused=false;
         startTime= Date.now() - elapsedTime;
-        intervalId= setInterval(updateTime, 1000);
+        intervalId= setInterval(updateTime, 50);
     }
 } );
 pauseBtn.addEventListener('click',() => {
@@ -34,20 +35,28 @@ resetBtn.addEventListener('click',()=>{
     hrs=0;
     mins=0;
     secs=0;
-    timeDisplay.textContent= "00:00:00";
+    timeDisplay.textContent= "00:00:00:000";
 });
 
 function updateTime(){
     elapsedTime= Date.now() - startTime; // time passed in milliseconds
+    milliseconds= Math.floor((elapsedTime%1000));
     secs= Math.floor((elapsedTime/1000)%60);
     mins= Math.floor(((elapsedTime/1000)/60)%60);
     hrs = Math.floor((((elapsedTime/1000)/60)/60)%60);
     hrs= pad(hrs);
     mins= pad(mins);
     secs= pad(secs);
-    timeDisplay.textContent= `${hrs}:${mins}:${secs}`;
+    milliseconds= pad_milli(milliseconds);
+    timeDisplay.textContent= `${hrs}:${mins}:${secs}:${milliseconds}`;
 
     function pad(unit){
         return (("0"+unit).length)>2 ? unit: "0"+unit;
+    }
+    function pad_milli(unit){
+        let str= "0"+unit;
+        if(str.length==3) return str;
+        if(str.length==2) return "0"+str;
+        return unit;
     }
 }
